b1658720068edb529421cd69462ef0ea94b8369e
2023317MaxDyson <154018224+2023317MaxDyson@users.noreply.github.com>
2024-04-17 19:59:50 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿
+class HowToPlayConnect4
+{
+    public HowToPlayConnect4 ( ) {
+
+        Console.WriteLine("How to play Connect 4: ");
+
+    }
+
+}
+
+
+class GameController
+{
+
+
+}
+
+
+ class GameModel
+{
+
+
+}
+
+
+class Players { 
+
+
+}
+
+class Player1 : Players
+{
+
+
+
+}
+
+
+class Player2 : Players
+{
+
+}
+
+
+
+
+namespace OOPProjectMaxDyson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine(" Connect 4 ");
+
+     
+          
+
+        }
+    }
+}
