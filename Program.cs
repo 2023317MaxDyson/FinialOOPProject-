9cd0b321dfa469f7d2a03ab8a49df2d64ff08b64
2023317MaxDyson <154018224+2023317MaxDyson@users.noreply.github.com>
2024-04-22 15:12:29 -0600

    Trying to make the connect 4 board

--- Program.cs
+++ Program.cs
@@ -4,34 +4,65 @@
 
 
 
+using System.ComponentModel.Design;
+
 namespace OOPProjectMaxDyson
 {
 
     using System.ComponentModel.Design;
+    using System.Runtime.InteropServices.Marshalling;
+    using System.Security.Cryptography.X509Certificates;
+
+    class Gamecontroller {
+
+ 
+    }
+
+
+
+    // Holds information about the game 
+
+
+   class Model
+    {
 
 
 
 
 
-
-class Gamecontroller {
-
-        
+        //  discs 
 
 
-}
 
 
- class Model { 
-        
-        
-      // seven-column, six-row vertically suspended grid
+
+        //   public string gameBoard() {
 
 
-}
+        // seven-column, six-row vertically suspended grid
+        // Counting by index: 6-column, 5-row vertically suspended grid
 
 
-abstract class Player { 
+
+        //  }
+
+
+
+
+        //    public bool startGame() {
+
+
+
+
+
+        //  }
+
+
+
+    }
+
+
+    abstract class Player { 
 
   
 
@@ -47,37 +78,45 @@ class Players : Player {
 
 
 
+
+
 }
 
 
- // Computer Player 
-
-/*
-class ComputerPlayer : Player
-{
-
-}
-
-*/
-
-class Interaction {
 
 
-       public Interaction() {
-            
+    // Computer Player 
 
-        }
-       
-    public void NeedHelp() { 
-
-     Console.WriteLine( "\n\n Connect 4 rules:  ");
-     Console.WriteLine("\n\n In Connect 4 their are two players that can pick their two symbols (X or O) as thier discs.\n ");
-     Console.WriteLine("Players take turns by dropping their discs in the Connect 4 game board.\n ");
-     Console.WriteLine("To win Connect 4 you must get a 4 in a row and it could be diagionial or horizontal or verticial.");
-  
-       
+    /*
+    class ComputerPlayer : Player
+    {
 
     }
+
+    */
+
+    class Interaction {
+
+
+        public Interaction() {
+
+
+        }
+
+
+        public static void NeedHelp() {
+
+            Console.WriteLine("\n\n Connect 4 rules:  ");
+            Console.WriteLine("\n\n In Connect 4 their are two players that can pick their two symbols (X or O) as thier discs.\n ");
+            Console.WriteLine("Players take turns by dropping their discs in the Connect 4 game board.\n ");
+            Console.WriteLine("To win Connect 4 you must get a 4 in a row and it could be diagionial or horizontal or verticial.");
+        }
+
+       public void exitGame()   {
+
+            Console.WriteLine("You have exited the game");
+
+            }
 
     
     
@@ -91,11 +130,8 @@ class Interaction {
     internal class Program
     {
         static void Main(string[] args)
+       
         {
-
-
-
-
             Console.WriteLine(" Welcome to the Connect 4 Game: ");
             Console.WriteLine("\n Type Help to look at how to play Connect 4 \n\n Type Start to start the game \n");
 
@@ -103,31 +139,57 @@ class Interaction {
 
 
 
+         string[,] gameBoard = new string[7, 6];
+
+
+
+
+
             // Help on how to play connect 4 
 
-            if (input == "Help") {
 
-                // Implemment a Interaction class object
+            if (input == "Help")   {
 
-                var Interact = new Interaction();
 
-                Interact.NeedHelp();
+                Interaction.NeedHelp();
 
 
 
             }
 
-
-
             // Start the game 
 
             else if (input == "Start") {
 
-                
+
+                for (int i = 0; i <= 7; i++)  {
+
+         
+                    for (int j = 0; j <= 6; j++) {
+
+                        gameBoard[i, j] = "#";
+
+
+                        Console.WriteLine("\n" + gameBoard[i, j]);
+
+                    }
+
+                    Console.WriteLine();
 
                 }
+              
+            }
 
 
+            // Exit the game 
+
+            else if (input == "Exit")  {
+
+                var Exit = new Interaction();
+                Exit.exitGame();
+            }
+
+        
         }
     }
 }
