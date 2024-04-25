e35a3d890fd9156ed488c826359d8355ad501ce5
2023317MaxDyson <154018224+2023317MaxDyson@users.noreply.github.com>
2024-04-24 23:17:40 -0600

    Done

--- Program.cs
+++ Program.cs
@@ -6,175 +6,290 @@
 
 
 
+using OOPProjectMaxDyson;
 using System.ComponentModel.Design;
+using System.Data;
+using System.Data.Common;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 
 namespace OOPProjectMaxDyson
 {
 
     using System.ComponentModel.Design;
+    using System.Runtime.CompilerServices;
     using System.Runtime.InteropServices.Marshalling;
     using System.Security.Cryptography.X509Certificates;
     using System.Transactions;
 
 
-
     // The Game controller 
 
-    // The Model class and the Player abstract class should  should communicate with the world via an object that is created in your controller class.
-
-    class Gamecontroller {
-
-        Gamecontroller player1;
 
 
-      
+    class GameController
+    {
+
+        public static bool Connect4Game()
+
+        {
+
+            // Create 2 player objects 
+
+            Player player1 = new UserPlayer("Player1", "X");
+            Player player2 = new UserPlayer("Player2", "0");
+
+            // Display the board 
+
+            Model.Board();
+
+            // Display  the buttons for each column 
+
+            Controller();
 
 
-        public static void Connect4Game() {
+            while (true)
+            {
+
+                int column = player1.GetNextMove() +1;
+
+                // Drop the disc in the selected column 
+
+                if (!Model.DropDisc(column, player1.Disc)) {
+
+                    Interaction.Error();
+
+                    continue;
 
 
-          Console.WriteLine("\n  Enter (1-7):  ");
+                }
 
 
-          Console.ReadLine();
+                 column = player2.GetNextMove() + 1;
+
+                if (!Model.DropDisc(column, player2.Disc))
+                {
+
+                    Interaction.Error();
+
+                    continue;
 
 
+                }
 
-
+            }
         }
 
+        public static void Controller()
+        {
 
-        public static void Controller(string buttons) {
 
-           Console.Write(buttons + " ");
-        
-        
+            // Create a list of buttons for each column 
+
+
+            List<string> ButtonsList = new List<string>();
+
+
+            ButtonsList.Add(" " + " " + " " + "1");
+            ButtonsList.Add("2");
+            ButtonsList.Add("3");
+            ButtonsList.Add("4");
+            ButtonsList.Add("5");
+            ButtonsList.Add("6");
+            ButtonsList.Add("7");
+
+            foreach (string buttons in ButtonsList)
+            {
+
+                Console.Write(buttons + " ");
+
+
+            }
 
         }
+    }
 
 
-
-
-
-
-}
 
 
 
     // Holds information of the  game 
 
-                                                                                                                                                                                            
-   class Model: Gamecontroller {
+
+    class Model
+    {
 
 
-        public Model()    {
 
 
-
-        }
-
-          public static void Board() {
-
+        public static void Board()
+        {
 
 
 
             // seven-column, six-row vertically suspended grid
-            // Counting by index: 6-column, 5-row vertically suspended grid
 
-            // Create the gameboa
+            // Counting by index: 7-column, 5-row vertically suspended grid
 
             //Empty disc symbol  #
-
 
             // Updating the gameboard that is created by a multidimensial array;
 
 
+            String[,] gameBoardarray = {
+
+           /* 0 */     {  "[", "#", "#", "#", "#", "#", "#", "#", "]", "\n"},
+          /* 1  */     {  "[", "#", "#", "#", "#", "#", "#", "#", "]", "\n"},
+          /* 2  */     {  "[", "#", "#", "#", "#", "#", "#", "#", "]" , "\n"},
+          /* 3 */      {  "[", "#", "#", "#", "#", "#", "#", "#", "]" , "\n"},
+          /*  4 */     {  "[", "#", "#", "#", "#", "#", "#", "#", "]" , "\n"},
+          /* 5 */      {  "[", "#", "#", "#", "#", "#", "#", "#", "]" , "\n"}
+
+        };                   //    1    2    3    4    5    6    7
 
 
+            foreach (string gameBoard in gameBoardarray)
+            {
 
-            String[,] gameBoardarray = { 
- 
-                {  "[", "#", "#", "#", "#", "#", "#",  "#", "]", "\n"}  ,
-                {  "[", "#", "#", "#", "#", "#", "#",  "#", "]", "\n"}  ,
-                {  "[", "#", "#", "#", "#", "#", "#",  "#", "]" , "\n"} ,
-                {  "[", "#", "#", "#", "#", "#", "#",  "#", "]" , "\n"} ,
-                {  "[", "#", "#", "#", "#", "#", "#",  "#", "]" , "\n"} ,
-                {  "[", "#", "#", "#", "#", "#", "#",  "#", "]" , "\n"} ,
-                {  "[", "#", "#", "#", "#", "#", "#",  "#", "]" , "\n"} 
-            };
-
-
-
-            foreach(string gameBoard in gameBoardarray  ) {
-
-                
-                Console.Write( " " + gameBoard);
+                Console.Write(" " + gameBoard);
 
             }
-
-
-
-
-    }
-
-
-
-    }
-
-
-    abstract class Player: Gamecontroller {
-        
-        public string Player1;
-
-        public string Player2;
-
-
-
-
-        public Player() {
-
-
-
-     }
-
-
-
-
-    }
-
-
-
-    // 1 or 2 players 
-    class Players : Player {
-
-
-
-
-}
-
-
-
-
-    // Computer Player 
-
-    /*
-    class ComputerPlayer : Player
-    {
-
-    }
-
-    */
-
-    class Interaction {
-
-
-        public Interaction() {
 
 
         }
 
 
-        public static void NeedHelp() {
+
+        public static bool DropDisc(int column, string disc)
+        {
+
+
+            String[,] gameBoardarray = {
+
+          /* 0 */     {  "[", "#", "#", "#", "#", "#", "#", "#", "]", "\n"},
+         /* 1  */     {  "[", "#", "#", "#", "#", "#", "#", "#", "]", "\n"},
+         /* 2  */     {  "[", "#", "#", "#", "#", "#", "#", "#", "]" , "\n"},
+         /* 3 */      {  "[", "#", "#", "#", "#", "#", "#", "#", "]" , "\n"},
+         /*  4 */     {  "[", "#", "#", "#", "#", "#", "#", "#", "]" , "\n"},
+           /* 5 */    {  "[", "#", "#", "#", "#", "#", "#", "#", "]" , "\n"}
+
+        };           //    1    2    3    4    5    6    7
+
+            
+       
+
+
+            // Iterates from the bottom row to the top row depending on what column you picked
+
+            for (int row = gameBoardarray.GetLength(0) - 1; row > 0; row--)
+            {
+
+                if (gameBoardarray[row, column - 1] == "#")
+                {
+
+
+                    // Drops a disc that is empty 
+
+                    gameBoardarray[row, column] = disc;
+
+                    return true;
+
+                }
+
+            }
+
+            // Column is full 
+
+            return false;
+
+        }
+
+    }
+
+
+    abstract class Player
+    {
+
+
+        public string Name { get; set; }
+        public string Disc { get; set; }
+
+
+        protected Player(string name, string disc)
+        {
+
+            Name = name;
+            Disc = disc;
+
+        }
+
+        public abstract int GetNextMove();
+
+
+    }
+
+    // 1 or 2 players 
+    class UserPlayer : Player
+    {
+
+        // Variables 
+
+
+        public UserPlayer(string name, string disc) : base(name, disc)
+        {
+
+
+
+
+        }
+
+        public override int GetNextMove()
+        {
+
+            int column = -1;
+            bool Isvalidinput = false;
+
+            while (!Isvalidinput)
+            {
+
+                Console.WriteLine( "\n" + $"{Name},  enter column number (1-7):");
+                string input = Console.ReadLine();
+
+
+                //  Select a column 
+
+                if (input.Length == 1 && input[0] >= '1' && input[0] <= '7')
+                {
+
+                    column = input[0] - '1';
+
+                    Isvalidinput = true;
+                }
+
+                else
+                {
+                    Interaction.Error();
+                }
+
+
+            }
+
+
+            return column;
+        }
+
+    }
+
+
+    class Interaction
+    {
+
+
+
+
+
+        public static void NeedHelp()
+        {
 
             Console.WriteLine("\n\n Connect 4 rules:  ");
             Console.WriteLine("\n\n In Connect 4 their are two players that can pick their two symbols (X or O) as thier discs.\n ");
@@ -182,33 +297,35 @@ namespace OOPProjectMaxDyson
             Console.WriteLine("To win Connect 4 you must get a 4 in a row and it could be diagionial or horizontal or verticial.");
         }
 
-       public void exitGame()   {
+        public void exitGame()
+        {
 
             Console.WriteLine("You have exited the game");
 
-            }
+        }
 
-    
-    
+        public static void Error()
+        {
+
+            Console.WriteLine("Error, please type a number between 1 to 7;");
+
+        }
 
 
-}
+    }
 
 
 
 
     internal class Program
     {
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
 
 
             Console.WriteLine(" Welcome to the Connect 4 Game: ");
-              Console.WriteLine("\n Type Help to look at how to play Connect 4 \n\n Type Start to start the game \n");
+            Console.WriteLine("\n Type Help to look at how to play Connect 4 \n\n Type Start to start the game \n");
 
-
-
-            // Using a list to create the buttons for the game board
-            // 7 buttons
 
 
             var input = Console.ReadLine();
@@ -219,7 +336,8 @@ namespace OOPProjectMaxDyson
             // Help on how to play connect 4 
 
 
-            if (input == "Help") {
+            if (input == "Help")
+            {
 
 
                 Interaction.NeedHelp();
@@ -228,50 +346,35 @@ namespace OOPProjectMaxDyson
             }
 
             // Start the game
-         
-            else if (input == "Start") {
+
+            else if (input == "Start")
+            {
 
 
+                // GameController object 
 
-                Model.Board();
-
-
-                // Using a list to create the buttons for the game board
-                // 7 buttons
-
-                List<string> ButtonsList = new List<string>();
-
-                ButtonsList.Add(" " + " " + " " + "1");
-                ButtonsList.Add("2");
-                ButtonsList.Add("3");
-                ButtonsList.Add("4");
-                ButtonsList.Add("5");
-                ButtonsList.Add("6");
-                ButtonsList.Add("7");
-
-                foreach (string buttons in ButtonsList)
-                {
-               
-                    Gamecontroller.Controller(buttons);
-
-                }
+                GameController.Connect4Game();
 
 
-                Gamecontroller.Connect4Game();
 
             }
 
 
+
             // Exit the game 
 
-            else if (input == "Exit")  {
+            else if (input == "Exit")
+            {
 
                 var Exit = new Interaction();
                 Exit.exitGame();
             }
 
-           
-        
+
+
+
         }
     }
 }
+
+
